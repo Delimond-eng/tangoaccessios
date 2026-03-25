6b35f065b8a247203cc811d16e5ddf797882c2f5
@Delimond <64663390+Delimond-eng@users.noreply.github.com>
2026-03-25 20:29:09 +0100

    Update main.dart
    commenter le corps tout entier pour test app ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,22 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text("APP OK")),
+      ),
+    );
+  }
+}
+/*import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
@@ -119,3 +137,4 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+*/
